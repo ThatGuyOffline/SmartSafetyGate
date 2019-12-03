8fecf484587fae8f696eafcbd8ebe2a33236d9af
Joseph Shonle <joseph_shonle@yahoo.com>
2019-12-03 08:07:45 -0500

    UI Updates

---
+++ Smart Safety Gate/DetailsTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsTableViewController.swift
+//  Smart Safety Gate
+//
+//  Created by Joseph Shonle on 11/5/19.
+//  Copyright © 2019 Joseph Shonle. All rights reserved.
+//
+
+import Foundation
